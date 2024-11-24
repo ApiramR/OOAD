1f7001e7f26d8cef34b9bf9fca1bc40031aa7b59
Apiram Rajamohan <rapiram31@gmail.com>
2024-11-24 23:36:56 +0530

    Kanchan's initial error solved

--- demo/src/main/java/com/example/demo/controller/DoctorController.java
+++ demo/src/main/java/com/example/demo/controller/DoctorController.java
@@ -5,14 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.model.Doctor;
 import com.example.demo.service.DoctorService;
 
 @Controller
-@RequestMapping("api/doctorDetails")
 public class DoctorController {
 
     @Autowired
@@ -25,7 +22,7 @@ public class DoctorController {
     }
 
     @GetMapping(value="/doctor")
-    public String getMethodName(@RequestParam String param) {
+    public String getMethodName() {
         return "dashboard.html";
     }
     

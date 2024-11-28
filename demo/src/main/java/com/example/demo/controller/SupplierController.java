a42e31b6638f83d4e7f89cff33daf32c4ff9b832
Apiram Rajamohan <rapiram31@gmail.com>
2024-11-28 13:40:20 +0530

    sajd

--- demo/src/main/java/com/example/demo/controller/SupplierController.java
+++ demo/src/main/java/com/example/demo/controller/SupplierController.java
@@ -1,29 +1,40 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.model.Inventory;
 import com.example.demo.model.Medicine;
+import com.example.demo.model.Patient;
+import com.example.demo.model.PatientComments;
+import com.example.demo.model.Prescription;
+import com.example.demo.model.Report;
 import com.example.demo.model.Supplier;
 import com.example.demo.service.ModelMapperUtil;
 import com.example.demo.service.SupplierService;
 
-@RestController
-@RequestMapping("/inventory")
+@Controller
 public class SupplierController {
 
 
@@ -41,7 +52,91 @@ public class SupplierController {
     @RequestMapping(value="/supplier",method = RequestMethod.GET)
     String PharmacyDashboard(Model model){
         if (Auth(model)) return "redirect:/login";
-        return "supplierInventory";
+        return "SupplierProfile";
+    }
+    @GetMapping(value="/supplier/{username}/profile")
+    public String patientProfile(Model model,@PathVariable String username){
+        if (Auth(model)) return "redirect:/login";
+        return "SupplierProfile";
+    }
+    @GetMapping(value="/supplier/{username}/reports")
+    public String reportDashboard(Model model,@PathVariable String username){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.getName().equals(username)){
+            return "redirect:/login?loginagain";
+        }
+        Supplier supplier = supplierService.getSupplierByUsername(username);
+        //List<Report>reports = supplier.getReports();
+        String reportpath = "/reports/";
+        initialize(username, model, supplier);
+        //model.addAttribute("orders",orders);
+        model.addAttribute("reportpath",reportpath);
+        return "Supplieriinventory.html";
+    }
+    @PostMapping(value="/supplier/{username}/reports")
+    public String supplierAddReports(Model model,@PathVariable String username, @RequestParam String title, @RequestParam MultipartFile file){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.getName().equals(username)){
+            return "redirect:/login?loginagain";
+        }
+        Supplier supplier = supplierService.getSupplierByUsername(username);
+        //String added = reportService.addReport(file,title,supplier);
+        model.addAttribute("success","success");  
+        //List<Report>order = supplier.orders();
+        String reportpath = "/reports/";
+        initialize(username, model, supplier);
+        //model.addAttribute("reports",orders);
+        model.addAttribute("reportpath",reportpath);
+        return "PatientReports.html";
+    }
+    @RequestMapping(value="/supplier/{username}/prescriptions")
+    public String prescriptionDashboard(Model model,@PathVariable String username){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.getName().equals(username)){
+            return "redirect:/login?loginagain";
+        }
+        Supplier supplier = supplierService.getSupplierByUsername(username);
+        //List<Prescription>prescriptions = supplier.getPrescriptions();
+        //List<PatientComments>comments = supplier.getComments();
+        initialize(username, model, supplier);
+        //model.addAttribute("comments",comments);
+        //model.addAttribute("prescriptions",prescriptions);//change it to prescriptions
+        return "SupplierOrders.html";
+    }
+     @PostMapping("/supplier/settings")
+    @ResponseBody
+    public ResponseEntity<?> PatientChangeSettings(@RequestParam Map<String, String> formData,@RequestParam(value = "profilepicture", required = false) MultipartFile file){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Update failed");
+        }
+        Map<String,String> response = new HashMap<String,String>();
+        String username = authentication.getName();
+        Supplier supplier = supplierService.getSupplierByUsername(username);
+        if (file != null && !file.isEmpty()){
+            System.out.println("Yes iam changing profile picture");
+            System.out.println(supplierService.Saveprofilepicture(file,supplier));
+        }
+        if (supplierService.updatePatient(formData,supplier)){
+            response.put("Fname",supplier.getFname());
+            response.put("Mname",supplier.getMname());
+            response.put("Lname",supplier.getLname());
+            return ResponseEntity.ok(response);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Update failed");
+        }
+    }
+        @GetMapping(value="/supplier/settings")
+    public String patientSettings(Model model){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null){
+            return "redirect:/login?loginagain";
+        }
+        String username = authentication.getName();
+        Supplier supplier = supplierService.getSupplierByUsername(username);
+        initialize(username, model, supplier);
+        return "SupplierSettings.html";
     }
 
     @PostMapping("/add")
@@ -159,12 +254,20 @@ public class SupplierController {
             String[] fields = supplierService.getFields();
             Map<String, Object> supplierDict = modelMapperUtil.mapFieldsToGetters(supplier, fields);
             supplierDict.put("profilepic",profilepicture);
-            model.addAttribute("pharmacy",supplierDict);
+            model.addAttribute("supplier",supplierDict);
         }
         else{
             return true;
         }
         return false;
+    }
+    public void initialize(String username,Model model,Supplier supplier){
+        String profilepicpath = "/images/";
+        String profilepicture = profilepicpath + supplier.getProfilepicture();
+        String[] fields = supplierService.getFields();
+        Map<String, Object> patientDict = modelMapperUtil.mapFieldsToGetters(supplier, fields);
+        patientDict.put("profilepic",profilepicture);
+        model.addAttribute("supplier",patientDict);    
     }
 
 }
--- demo/src/main/java/com/example/demo/service/SupplierService.java
+++ demo/src/main/java/com/example/demo/service/SupplierService.java
@@ -1,12 +1,20 @@
 package com.example.demo.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.demo.model.Patient;
 import com.example.demo.model.Supplier;
 import com.example.demo.repo.SupplierRepo;
 
@@ -20,6 +28,8 @@ public class SupplierService {
     SupplierRepo rep;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Value("${file.upload-dir2}")
+    private String imageuploaddir;
 
     public boolean validate(String username, String password){
         Supplier supplier = rep.findByUsername(username);
@@ -53,7 +63,66 @@ public class SupplierService {
     public Supplier getSupplierByUsername(String username){
         return rep.findByUsername(username);
     }
-
+    public String Saveprofilepicture(MultipartFile file, Supplier supplier){
+        String currentReportpath = imageuploaddir + supplier.getProfilepicture();
+            File currentpicture = new File(currentReportpath);
+            if (currentpicture.exists()){
+                System.out.println("Why its not deletingggg");
+                currentpicture.delete();
+            }
+            long l = System.currentTimeMillis();
+            String s = l + "";
+            String filename = 'a' + s + '_' + file.getOriginalFilename();
+            System.out.println(file.getOriginalFilename());
+            File directory = new File(imageuploaddir);
+            if (!directory.exists()){
+                directory.mkdirs();
+            }
+            try{
+                File destinationFile = new File(directory,filename);
+                supplier.setProfilepicture(filename);    
+                file.transferTo(destinationFile);
+                rep.save(supplier);
+                return "/images/" + filename;
+                
+            }catch(IOException e){
+                e.printStackTrace();
+                return "/images/defaultpp.jpg";
+            }
+    }
+    public Boolean updatePatient(Map<String,String>formData,Supplier supplier){
+        try{
+            for (Map.Entry<String, String> column : formData.entrySet()) {
+                String fieldName = column.getKey();
+                Object fieldValue = column.getValue();
+                if (fieldName.equals("profilepicture")){
+                    continue;
+                }
+                if (fieldValue == null)continue;
+                if (fieldValue.equals(""))continue;
+                System.out.println(fieldName + " " + (String)fieldValue);
+                    String methodName = "set" + fieldName.substring(0,1).toUpperCase() + fieldName.substring(1);
+                    String res = (String)fieldValue;
+                    if (fieldName.equals("Password")){
+                        System.out.println(res);
+                        res = passwordEncoder.encode(res);
+                        System.out.println(res);
+                    }
+                    else if (fieldName.equals("height") || fieldName.equals("weight")){
+                        Method setter = Patient.class.getMethod(methodName, Double.class);
+                        setter.invoke(supplier,Double.parseDouble(res));
+                        continue;    
+                    }
+                    Method setter = Patient.class.getMethod(methodName, String.class);
+                    setter.invoke(supplier,res);
+            }
+        }catch(NoSuchMethodException | IllegalAccessException | InvocationTargetException e){
+            e.printStackTrace();
+            return false;
+        }
+        rep.save(supplier);
+        return true;
+    }
     public void deleteSupplier(int supplierID){
         rep.deleteById(supplierID);
     }

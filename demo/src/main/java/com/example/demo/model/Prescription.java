25b7f79a06a7a68c18312f9d309fdce1a056b5bc
Apiram Rajamohan <rapiram31@gmail.com>
2024-11-28 09:37:43 +0530

    Updated docker and all

--- demo/src/main/java/com/example/demo/model/Prescription.java
+++ demo/src/main/java/com/example/demo/model/Prescription.java
@@ -8,7 +8,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -29,7 +28,7 @@ public class Prescription {
     @Column(name="presid")
     private Long presID;
 
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "MID", nullable = false)
     private Medicine meds;
 
